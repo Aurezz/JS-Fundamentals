e6fd56a585a662ec072c0a56f455f8f243c37051
Aurezz <31102170+Aurezz@users.noreply.github.com>
2017-12-25 12:38:15 +0500

    Add files via upload

---
+++ Chapter9.js
@@ -0,0 +1,12 @@
+// Few user-interface functions
+alert("This shows up a modal window");
+// promt
+// result = prompt(title[, default]);
+let namee = prompt('What is your name?', '');
+
+alert(`Your name is ${namee}!`); // You are 100 years old!
+
+// confirm
+// result = confirm(question);
+let isName = confirm("Is your name John?");
+alert(isName);
